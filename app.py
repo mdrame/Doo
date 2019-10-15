24d2fe2539cc114c9121e51045c0608ccebc4715
mohammed <mdrame113@gmail.com>
2019-10-14 17:37:20 -0700

    Home and create logic done

--- app.py
+++ app.py
@@ -6,10 +6,24 @@ app = Flask(__name__)
 
 
 # This is the home route
-@app.route('/')
-def homerRoute():
+@app.route('/', methods=['POST', 'GET'])
+def homeRoute():
 
-    return render_template('index.html')
+# get data from createAndEdit route to be display in index.html
+    if request.method == "POST":
+        form_Infos = request.form
+
+    return render_template('index.html', form_Infos=form_Infos)
+
+
+# forms route " this route  connects from a href link in the index.html"
+# https://www.tutorialspoint.com/flask/flask_sending_form_data_to_template.htm
+@app.route('/createAndEdit', methods=['POST', 'GET'])
+def createAndEdit():
+
+
+    return render_template("createAndEdit.html" )
+
 
 
 
