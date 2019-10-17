16e15d10a4986f59a5d946438906420cb67db174
mohammed <mdrame113@gmail.com>
2019-10-17 10:43:12 -0700

    view notes successfully in index.html from creat&Edit.html

--- app.py
+++ app.py
@@ -11,8 +11,22 @@ database = list()
 def homeRoute():
 
 
+    
+
 
     return render_template('index.html')
+
+
+# forms route " this route  connects from a href link in the index.html"
+# https://www.tutorialspoint.com/flask/flask_sending_form_data_to_template.htm
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+
+    
+
+
+    return render_template("createAndEdit.html" )
+
 
 #Rout shoes ever listing created 
 @app.route('/notes', methods=['POST', 'GET'])
@@ -29,26 +43,20 @@ def notes():
         # print(form_infos['action'])
         
 
-    return render_template('index.html', form_infos=form_infos)
+        return render_template('index.html', form_infos=form_infos)
+
 
 #View todo list item route
 @app.route('/view')
 def view():
 
 
+
     return render_template('view.html')
 
 
 
-# forms route " this route  connects from a href link in the index.html"
-# https://www.tutorialspoint.com/flask/flask_sending_form_data_to_template.htm
-@app.route('/create', methods=['POST', 'GET'])
-def create():
 
-    
-
-
-    return render_template("createAndEdit.html" )
 
 
 
